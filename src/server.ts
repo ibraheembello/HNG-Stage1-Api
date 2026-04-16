3e1bc15e9084124393c3f6b9339298881c6a3b0a
ibraheembello <belloibrahimolawle@gmail.com>
2026-04-16 02:58:18 +0100

    fix: update server file paths in Vercel configuration and ensure server listens only in non-production environments

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import app from "./app";
 
-// It will use the PORT from your .env file, or default to 3000
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is flying on http://localhost:${PORT}`);
-  console.log(`👉 Test the endpoint at http://localhost:${PORT}/api/profiles`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is flying on http://localhost:${PORT}`);
+    console.log(`👉 Test the endpoint at http://localhost:${PORT}/api/profiles`);
+  });
+}
+
+export default app;
